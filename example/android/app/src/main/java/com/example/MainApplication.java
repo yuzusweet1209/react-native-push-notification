f9c1a92b5976e344fe1940af4bf1363748477e39
yuzusweet1209 <e0134101@u.nus.edu>
2021-08-19 10:15:24 +0800

    set up for notification

--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
@@ -26,7 +27,10 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          return packages;
+            return Arrays.<ReactPackage>asList(
+                    new MainReactPackage(),
+                    new ReactNativePushNotificationPackage() // <---- Add the Package
+            );
         }
 
         @Override
